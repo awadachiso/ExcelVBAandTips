0a80471f7e8eae4d1d5ea231636a3bad6f396ac1
awadachiso <seitakaawadachiso--69420.outlook.jp>
2021-05-11 18:22:16 +0900

    Support for multiple criteria

--- extractValuesWithKey.vb
+++ extractValuesWithKey.vb
@@ -9,3 +9,6 @@
  */
 
 =IFERROR(INDEX(Table[values],SMALL(IF(Table[Keys]=key,ROW(Table[values])-ROW(Table[[#Headers],[values]])),ROW()-ROW(BaseCell)+1)),"")
+
+'# For multiple criteria (e.g. key_1, key_2), Connect with * asterisk
+=IFERROR(INDEX(Table[Values],SMALL(IF((Table[Keys1]=key_1)*(Table[Keys2]=key_2),ROW(Table[Values])-ROW(Table[[#Headers],[Values]])),ROW()-ROW(BaseCell)+1)),"")
