fe1654119a9bde561d49f35cb94fdb87f09836f5
awadachiso <47885670+awadachiso@users.noreply.github.com>
2021-08-09 19:33:11 +0900

    Update extractValuesWithKey.vb
    add non-Table Version

--- extractValuesWithKey.vb
+++ extractValuesWithKey.vb
@@ -12,6 +12,10 @@
 
 =IFERROR(INDEX(Table[values],SMALL(IF(Table[Keys]=key,ROW(Table[values])-ROW(Table[[#Headers],[values]])),ROW()-ROW(BaseCell)+1)),"")
 
+'# not Table Version
+'destCell is cell that contains below arrayformula
+=IFERROR(INDEX($B$2:$B$11,SMALL(IF($A$2:$A$11=TRUE,ROW($B$2:$B$11)-ROW($B$1)),ROW()-ROW(destCell)+1)),"")
+
 '# For multiple criteria (e.g. key_1, key_2), Connect with * asterisk
 =IFERROR(INDEX(Table[Values],SMALL(IF((Table[Keys1]=key_1)*(Table[Keys2]=key_2),ROW(Table[Values])-ROW(Table[[#Headers],[Values]])),ROW()-ROW(BaseCell)+1)),"")
 
