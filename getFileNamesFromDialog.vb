f2644837e746c68d963468b086dfc87b229ce302
awadachiso <47885670+awadachiso@users.noreply.github.com>
2022-06-02 16:53:25 +0900

    Create getFileNamesFromDialog.vb
    init

---
+++ getFileNamesFromDialog.vb
@@ -0,0 +1,36 @@
+Option Explicit
+
+Function getFileNamesFromDialog()
+  Dim myFiles As Variant
+  Dim f As Variant
+  
+  ChDir "C:\Users\Kan\OneDrive" 'select default dir
+
+  myFiles = Application.GetOpenFilename( _
+    FileFilter:="Excel (*.xlsx;*.xls;*.xls),*.xlsx;*.xls;*.xls, CSV (*.csv),*.csv", _
+    MultiSelect:=True)
+  
+  'Check if the file was not selected
+  If isEmpty(myFiles) Then
+    getFileNamesFromDialog = myFiles
+  Else
+    'IF Not Selected, returns empty Array
+    getFileNamesFromDialog = Array()
+  End If
+End Function
+
+
+Function isEmpty(arr)
+  isEmpty = IsArray(arr)
+End Function
+
+
+Sub test()
+  Dim files As Variant
+  Dim f As Variant
+  files = getFileNamesFromDialog()
+  
+  For Each f In files
+    Debug.Print f
+  Next
+End Sub
